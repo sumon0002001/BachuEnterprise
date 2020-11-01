d0e2532c537795149edd66e284cc208a84cf441d
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2020-11-02 02:19:27 +0600

    fixed index file

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-var slideIndex = 0;
-carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+var dt = new Date();
+document.getElementById("date").innerHTML = dt.toLocaleTimeString();
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
+
+// MDB Lightbox Init
+
+
+function switchStyle() {
+  if (document.getElementById('styleSwitch').checked) {
+    document.getElementById('gallery').classList.add("custom");
+    document.getElementById('exampleModal').classList.add("custom");
+  } else {
+    document.getElementById('gallery').classList.remove("custom");
+    document.getElementById('exampleModal').classList.remove("custom");
   }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
